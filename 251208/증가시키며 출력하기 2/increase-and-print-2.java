d3e0a8e8849fac9cb2ac8d541b23397b01165650
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-08 21:26:08 +0900

    Title: 증가시키며 출력하기 2, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251208/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    int N = 10;
+    while(N < 27) {
+        System.out.print(N + " ");
+        N++;
+    }
+    }
+}
