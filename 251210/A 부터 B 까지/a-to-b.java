1d2d1f5cbcf0e158001d98153a0d48cd8a914e69
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-10 00:27:43 +0900

    Title: A 부터 B 까지, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251210/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+
+    while(A <= B) {
+        System.out.print(A + " ");
+
+        if(A % 2 != 0) 
+            A = A * 2;
+        else if(A % 2 == 0)
+            A = A + 3;
+
+    }
+    }
+}
