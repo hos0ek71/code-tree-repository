26ba8b26da02c10f0caa6080f833e911eacde4e4
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-10 17:53:22 +0900

    Title: 5의 배수의 합, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 251210/5의 배수의 합/sum-of-specific-condition-multiple.java
+++ 251210/5의 배수의 합/sum-of-specific-condition-multiple.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    int sum = 0;
+
+    boolean AB = (A >= B);
+
+    if(AB) {
+        for(int i = B; i <= A; i++){
+            if(B % 5 == 0){
+                sum = sum + B;
+            }
+            B++;
+        }
+        System.out.print(sum);
+    }
+    else {
+        for(int i = A; i <= B; i++){
+            if(A % 5 == 0){
+                sum += A;
+            }
+            A++;
+        }
+        System.out.print(sum);
+    }
+
+    
+
     }
 }
