bb54530e0a383a3baf099abf7c7e2cff804e630e
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-10 19:40:46 +0900

    Title: 조건에 부합하는 수, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251210/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int i = 1; 
+
+
+    while(i <= A) {
+        if((i % 2 == 0 && i % 4 != 0) || (i / 8) % 2 == 0) || (i % 7 < 4))  {
+            i++;
+            continue;
+        }
+        System.out.print(i + " ");
+        i++;
+    }
+
+    }
+}
