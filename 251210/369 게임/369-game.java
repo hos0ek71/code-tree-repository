abf9d62c2260b7d0e8d47e2d3e21a7eb0efe1290
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-10 15:45:22 +0900

    Title: 369 게임, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

--- 251210/369 게임/369-game.java
+++ 251210/369 게임/369-game.java
@@ -6,7 +6,8 @@ public class Main {
     int N = sc.nextInt();
     int i = 1;
     while(i <= N) {
-        if( i % 3 == 0 || (i % 10 == 3 || i % 10 == 6 || i % 10 == 9)) {
+        if( i % 3 == 0 || (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) || (i / 10 == 3 || i / 10 == 6 || i / 10 == 9)) {
+
             System.out.print(0 + " ");
         }
         else {
