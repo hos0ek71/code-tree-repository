9f82dea9bb77895184a4b257c5934ad819250cbf
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-10 22:30:51 +0900

    Title: 3N + 1 수열, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251210/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int count = 0;
+
+    while(true) {
+    
+        if(N == 1) break;
+        if(N % 2 == 0) {
+            N = N / 2;
+        }
+        else {
+            N = N * 3 + 1;
+        }
+        count++;
+    }
+    System.out.print(count);
+    }
+}
