345288271bf4e35ca8b33de8ba22da9f0d2735f3
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-10 15:41:53 +0900

    Title: 369 게임, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251210/369 게임/369-game.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int i = 1;
+    while(i <= N) {
+        if( i % 3 == 0 || (i % 10 == 3 || i % 10 == 6 || i % 10 == 9)) {
+            System.out.print(0 + " ");
+        }
+        else {
+            System.out.print(i + " ");
+        }
+        i++;
+    }
+    
+    }
+}
+
