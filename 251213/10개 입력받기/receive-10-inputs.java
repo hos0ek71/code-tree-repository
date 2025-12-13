c16a0894e35ce0deb0e8af8e6e0fa5456666ccf8
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-13 17:30:07 +0900

    Title: 10개 입력받기, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251213/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+    Scanner sc= new Scanner(System.in);
+    int[] arr = new int[10];
+    int sum = 0; 
+    int count = 0;
+
+    for(int i = 0; i < 10; i++) {
+        arr[i] = sc.nextInt();
+        if(arr[i] == 0 ){
+            break;
+        }
+        sum += arr[i];
+        count++;
+    }
+
+    double avg =(double) sum / count;
+    System.out.print(sum + " " + avg);
+    }
+}
