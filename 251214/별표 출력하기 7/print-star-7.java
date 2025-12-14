e02a3486c382d7340d7d5a9cb93775fea4c271d8
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-14 15:41:23 +0900

    Title: 별표 출력하기 7, Time: 130ms, Memory: 10MB, Status: Passed - Codetree

--- 251214/별표 출력하기 7/print-star-7.java
+++ 251214/별표 출력하기 7/print-star-7.java
@@ -7,7 +7,7 @@ public class Main {
 
     for(int i = 0; i < N; i++) {
         for(int j = 0; j <= i; j++) {
-            System.out.print("*");
+            System.out.print("*" + " ");
         }
         System.out.println();
     }
