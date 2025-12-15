abf38176fb5376c23c77eedcb4f9ef47b17f7a04
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-15 13:51:02 +0900

    Title: 두 개의 직각삼각형, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251215/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    
+    int n = 3;
+    for(int i = 0; i < n; i--){
+        for(int j = 0; j < i * 2; j++){
+            System.out.print("*")
+        }
+    }
+    }
+}
