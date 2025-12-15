fee76d3d53f42be7d8b6c1575fb299698fc4fc82
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-15 16:20:49 +0900

    Title: 별 그리기, Time: 178ms, Memory: 11MB, Status: Passed - Codetree

--- 251215/별 그리기/star-drawing.java
+++ 251215/별 그리기/star-drawing.java
@@ -1,19 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-    
-    int n = 3; 
-
-    int count = n;
+        
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j <= n-i;  j++) {
+        for(int j = 0; j < n-i-1;  j++) {
             System.out.print(" ");
         }
         for(int k = 0; k < (2 * i) + 1; k++) {
-            System.out.print("* ");
+            System.out.print("*");
         }
         System.out.println();
     }
+
+    for(int i = n-1; i > 0; i--) {
+        for(int j = 1; j <= n -i;  j++) {
+            System.out.print(" ");
+        }
+        for(int l = 2 * i -1 ; l > 0 ; l--) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
     
     }
 }
