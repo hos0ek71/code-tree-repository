f69ace92cba8400503b4b52bf7e08096b14531e3
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-16 19:19:06 +0900

    Title: 규칙을 찾아 출력하기, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 251216/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 251216/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -7,7 +7,7 @@ public class Main {
 
         for (int i = 1; i <= N; i++) {          // i: 행
             for (int j = 1; j <= N; j++) {      // j: 열
-                if (i == 1 || i == N || j == N || j <= i - 1) {
+                if (i == 1 || i == N || i > j || j == N ) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
