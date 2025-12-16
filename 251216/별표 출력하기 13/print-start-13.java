051fe01fd7b900a3e57143aa4d7c4d12b6dfa2c0
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-16 15:03:39 +0900

    Title: 별표 출력하기 13, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251216/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    
+    int n = 4;
+    
+    for(int i = n; i > 0; i--) {
+        if(i % 2 == 0) {
+            for(int j =  n-2 + (i /2)  ; j > 0; j-- ){
+                System.out.print("* ");
+            }
+        }
+        else {
+            for(int j = 0; j < n - (i / 2)- 2; j++) {
+                System.out.print("* ");
+            }
+        }
+        System.out.println();
+    }
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 0) {
+           for(int j =  n-2 + (i /2)  ; j > 0; j-- ){
+                System.out.print("* ");
+            }
+        }
+        else {
+            for(int j = 0; j < n - (i / 2)- 2; j++) {
+                System.out.print("* ");
+            }
+        }
+        System.out.println();
+    }
+    }
+}
+
