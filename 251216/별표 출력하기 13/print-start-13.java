367c6649702daee49e5fb5ce3b4f2da78a2e8528
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-16 15:26:27 +0900

    Title: 별표 출력하기 13, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

--- 251216/별표 출력하기 13/print-start-13.java
+++ 251216/별표 출력하기 13/print-start-13.java
@@ -1,37 +1,40 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Please write your code here.
-    Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    
-    for(int i = n; i > 0; i--) {
-        if(i % 2 == 0) {
-            for(int j =  n-2 + (i /2)  ; j > 0; j-- ){
-                System.out.print("* ");
-            }
-        }
-        else {
-            for(int j = 0; j < n - (i / 2)- 2; j++) {
-                System.out.print("* ");
-            }
-        }
-        System.out.println();
-    }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 예: 5
 
-    for(int i = 1; i <= n; i++) {
-        if(i % 2 == 0) {
-           for(int j =  n-2 + (i /2)  ; j > 0; j-- ){
-                System.out.print("* ");
+        // [상단부] 0부터 n-1까지 (총 n줄)
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                // 짝수 줄: N에서 줄어드는 별 (N, N-1, N-2...)
+                for (int j = 0; j < n - (i / 2); j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                // 홀수 줄: 1에서 늘어나는 별 (1, 2, 3...)
+                for (int j = 0; j < (i / 2) + 1; j++) {
+                    System.out.print("* ");
+                }
             }
+            System.out.println();
         }
-        else {
-            for(int j = 0; j < n - (i / 2)- 2; j++) {
-                System.out.print("* ");
+
+        // [하단부] n-1부터 0까지 거꾸로 (총 n줄) -> 완벽한 대칭
+        for (int i = n - 1; i >= 0; i--) {
+            if (i % 2 == 0) {
+                // 상단부와 로직 100% 동일
+                for (int j = 0; j < n - (i / 2); j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                // 상단부와 로직 100% 동일
+                for (int j = 0; j < (i / 2) + 1; j++) {
+                    System.out.print("* ");
+                }
             }
+            System.out.println();
         }
-        System.out.println();
-    }
     }
 }
-
