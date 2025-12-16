b84bf0af18f9b3d0968c73a37c971759a3e96adb
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-16 19:41:20 +0900

    Title: 홀수로 이루어진 정사각형, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251216/홀수로 이루어진 정사각형/square-with-odd-numbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner
+
+
+    int n = sc.nextInt();
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            System.out.print(11 + 2*j + " ");
+        }
+        System.out.println();
+    }
+    }
+}
