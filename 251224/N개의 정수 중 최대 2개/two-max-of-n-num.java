ec0e47f7bf1eef10f625f5ec7d2928e6832b69fa
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-24 20:50:29 +0900

    Title: N개의 정수 중 최대 2개, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251224/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int maxVal = Integer.MIN_VALUE;
+        int secondVal = Integer.MIN_VALUE;
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[2];
+
+        for(int i = 0; i < N; i++){
+            
+            int A = sc.nextInt();
+            if(A > maxVal) {
+                maxVal = A;
+            }
+            else{
+                if(A > secondVal) {
+                    secondVal = A ;
+                }
+            }
+        }
+        System.out.printf("%d %d", maxVal, secondVal);
+
+
+    }
+}
