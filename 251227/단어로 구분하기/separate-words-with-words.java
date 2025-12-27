4fc1af9e3d1efa1cbef97ea89a3d7cfc381a5d52
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-27 17:01:47 +0900

    Title: 단어로 구분하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251227/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String[] arr = str.split(" ");
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+
+
+    }
+}
