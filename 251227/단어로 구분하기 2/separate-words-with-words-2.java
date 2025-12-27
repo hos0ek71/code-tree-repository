f76159b824ddc03ef24b5aaf70a9bfb99ce215f2
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-27 17:04:12 +0900

    Title: 단어로 구분하기 2, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251227/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+
+    String[] arr = str.split(" ");
+
+    for(int i = 0; i < arr.length; i++){
+        if(i % 2 == 0) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    }
+}
