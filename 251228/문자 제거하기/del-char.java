7def83f3603f0f7b6fd2acdb6ee6bfb51f50aca8
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-28 23:51:23 +0900

    Title: 문자 제거하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 251228/문자 제거하기/del-char.java
+++ 251228/문자 제거하기/del-char.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        if (!sc.hasNextLine()) return; // 첫 줄 입력 확인
         String str = sc.nextLine();
-
         String result = str;
-        String str1 = "";
-        String str2 = "";
-        while(result.length() > 1){
+
+        // 숫자가 있고 문자열 길이가 1보다 클 때만 실행
+        while (result.length() > 1 && sc.hasNextInt()) { 
             int N = sc.nextInt();
-            if(N > 0 && N < result.length()) {
-                str1 = result.substring(0, N);
-                str2 = result.substring(N+1);
+            
+            if (N > 0 && N < result.length()) {
+                String str1 = result.substring(0, N);
+                String str2 = result.substring(N + 1);
                 result = str1 + str2;
-                System.out.println(result);
-            }
-            else if(N > result.length()) {
-                str1= result.substring(0, result.length()-1);
-                result = str1;
-                System.out.println(result);
-            }
-            else {
-                str1 = result.substring(1,result.length());
-                result = str1;
-                System.out.println(result);
+            } else if (N >= result.length()) {
+                result = result.substring(0, result.length() - 1);
+            } else { // N <= 0 인 경우
+                result = result.substring(1);
             }
             
+            System.out.println(result);
         }
+        sc.close();
     }
 }
