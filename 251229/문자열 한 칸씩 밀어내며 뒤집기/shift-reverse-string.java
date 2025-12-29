44a0a307db78c1f47e5ebafc79d7466a82a4885c
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-29 14:03:23 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 868ms, Memory: 176MB, Status: Memory Limit Exceed - Codetree

--- 251229/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 251229/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -3,31 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int N = sc.nextInt();
-        
-        int len = str.length();
-        String result = str;
+
+        String result = sc.next(); // 초기 문자열
+        int N = sc.nextInt();      // 연산 횟수
+        int len = result.length();
 
         for (int i = 0; i < N; i++) {
             int Q = sc.nextInt();
-            
+
             if (Q == 1) {
+                // 왼쪽 회전: 1번 인덱스부터 끝까지 + 0번 인덱스
                 result = result.substring(1) + result.substring(0, 1);
             } 
             else if (Q == 2) {
-                result = result.substring(len - 1, len) + result.substring(0, len - 1);
+                // 오른쪽 회전: 마지막 글자 + 0번부터 마지막 직전까지
+                result = result.substring(len - 1) + result.substring(0, len - 1);
             } 
             else if (Q == 3) {
-                // 원래 코드의 substring 방식을 유지한 뒤집기
-                String sum = ""; // 1. 결과를 담을 변수를 루프 밖에서 선언
-                for (int j = 0; j < len; j++) {
-                    // 2. 뒤에서부터 한 글자씩 잘라서 sum에 더함
-                    sum += result.substring(len - j - 1, len - j);
+                // 문자열 뒤집기 (반복문 사용)
+                String reversed = ""; 
+                for (int j = len - 1; j >= 0; j--) {
+                    // 뒤에서부터 한 글자씩 가져와서 새로운 문자열에 더함
+                    reversed += result.charAt(j);
                 }
-                result = sum; // 3. 뒤집힌 문자열을 result에 다시 저장
+                result = reversed; // 뒤집힌 문자열을 다시 result에 저장
             }
-            
+
             System.out.println(result);
         }
         sc.close();
