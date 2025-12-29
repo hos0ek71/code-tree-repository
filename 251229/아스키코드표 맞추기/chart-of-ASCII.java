3a62c30e1cf846ed5cb08ce3d71d0ab93a189e4a
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2025-12-29 15:10:51 +0900

    Title: 아스키코드표 맞추기, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251229/아스키코드표 맞추기/chart-of-ASCII.java
+++ 251229/아스키코드표 맞추기/chart-of-ASCII.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    
     for(int i = 0; i < 5; i++){
         int A = sc.nextInt();
         System.out.print((char)A + " ");
