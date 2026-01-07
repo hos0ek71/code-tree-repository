be9a9f9166cea534e3ede22b3a501f31eaa028f0
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-07 23:57:05 +0900

    Title: 함수를 이용한 온전수 판별, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260107/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -0,0 +1,23 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+def onjunsu(n):
+    
+    if n % 2 == 0:
+        return False
+    if n % 10 == 5:
+        return False
+    if(n % 3 == 0 and n % 9 != 0):
+        return False
+
+    return True
+
+cnt = 0 
+for i in range(a, b+1):
+    if onjunsu(i):
+        cnt += 1
+
+print(cnt)
+
+    
