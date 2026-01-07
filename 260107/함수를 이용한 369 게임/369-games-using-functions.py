26bcfebf67c575889c772ab68729451e9e6b0dd9
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-07 17:06:40 +0900

    Title: 함수를 이용한 369 게임, Time: 82ms, Memory: 17MB, Status: Passed - Codetree

--- 260107/함수를 이용한 369 게임/369-games-using-functions.py
+++ 260107/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,22 +1,15 @@
 a, b = map(int, input().split())
 
-# Please write your code here.
-
-def one_num(i):
-    return (i % 10 == 3 or i % 10 == 6 or i % 10 == 9)
-
-def ten_num(i):
-    return (i // 10 == 3 or i // 10 == 6 or i // 10 == 9)
-
-def magic_num(i):
-    return (ten_num(i) or one_num(i))
-    
-def mul_three_num(i):
-    return (i % 3 == 0)
-
 cnt = 0
-for i in range(a,b+1):
-    if magic_num(i) or mul_three_num(i):
+for i in range(a, b + 1):
+    # 1. 숫자를 문자열로 변환 (예: 135 -> "135")
+    s = str(i)
+    
+    # 2. '3', '6', '9'가 문자열 안에 있는지 확인하거나 3의 배수인지 확인
+    has_369 = ('3' in s) or ('6' in s) or ('9' in s)
+    is_mul_3 = (i % 3 == 0)
+    
+    if has_369 or is_mul_3:
         cnt += 1
 
 print(cnt)
