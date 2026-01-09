5fdf6071b4b4c87673b66f6727585a5e0bae97b2
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-09 15:29:34 +0900

    Title: 두 정수에 대한 연산값, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260109/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+# Please write your code here.
+
+def ab(a, b) :
+    if a >= b:
+        return a + 25, b * 2
+    else:
+        return a * 2, b + 25
+
+a, b= ab(a,b)
+print(a, b, end=" ")
