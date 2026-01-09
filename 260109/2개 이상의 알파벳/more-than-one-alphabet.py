53226fc1dd6e73c64ee1101a210e91f27113ff9a
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-09 15:55:33 +0900

    Title: 2개 이상의 알파벳, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260109/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,28 @@
+A = input()
+
+# Please write your code here.
+leng = len(A)
+
+def dif2(A, leng):
+
+    fisrt = A[0]
+    cnt = 0
+    
+    for i in range(1, leng):
+
+        if A[i] != fisrt:
+
+            cnt += 1
+
+        if cnt > 2:
+            return True
+        
+        first = A[i]
+    
+    return False
+
+if dif2(A,leng):
+    print("Yes")
+else:
+    print("No")
+
