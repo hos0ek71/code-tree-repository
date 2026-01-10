f79f36da89f1446ad8eb9b82f1746d8186e3bda1
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-10 23:06:52 +0900

    Title: 특정 구간의 합, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260110/특정 구간의 합/sum-of-specific-intervals.py
+++ 260110/특정 구간의 합/sum-of-specific-intervals.py
@@ -2,22 +2,25 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 queries = [tuple(map(int, input().split())) for _ in range(m)]
+total = 0
+
+
+def findIdx(a1, a2):
+    global total
+    total = 0; 
+    for i in range(a1-1 , a2):
+        total += arr[i]
+
 
 def find_range_sum():
     # m개의 쿼리를 하나씩 확인
     for i in range(m):
         # 1. 튜플에서 a1, a2를 각각 꺼냄
         a1, a2 = queries[i]
-        
-        # 2. 구간 합을 구할 변수 초기화
-        total = 0
-        
-        # 3. a1번째부터 a2번째까지 반복 (인덱스는 -1 해줘야 함)
-        for idx in range(a1 - 1, a2):
-            total += arr[idx]
-        
-        # 4. 결과 출력
-        print(total)
 
-# 함수 실행
+        findIdx(a1, a2)
+        print(total)
+      
+
 find_range_sum()
+
