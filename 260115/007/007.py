9196e30a104ee4795db2a86541d0b64a3572407b
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-15 21:08:32 +0900

    Title: 007, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260115/007/007.py
+++ 260115/007/007.py
@@ -1,16 +1,9 @@
 secret_code, meeting_point, time = input().split()
 time = int(time)
 
-# Please write your code here.
 
-class secret:
-    def __init__(self, code, place, time):
-        self.code = code
-        self.place = place
-        self.time = time
-    
-secret = secret(secret_code, meeting_point, time)
+secret = (secret_code, meeting_point, time)
 
-print(f"secret code : {secret.code}")
-print(f"meeting point : {secret.place}")
-print(f"time : {secret.time}")
+one, two, three= secret
+
+print(f"secret code : {one}")
