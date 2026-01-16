1f4a870aca38277b93742f29362c5a675749cb47
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-16 20:57:34 +0900

    Title: 총점 비교, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260116/총점 비교/compare-total-points.py
@@ -0,0 +1,20 @@
+class StudentGrade:
+    def __init__(self, name, kor, eng, math):
+        self.name = name 
+        self.kor = int(kor)
+        self.eng = int(eng)
+        self.math = int(math)
+
+n = int(input())
+
+
+arr = [input().split() for _ in range(n)]
+grade = [StudentGrade(name, kor, eng, math) for name, kor, eng, math in arr ]
+
+
+grade.sort(key = lambda x : (x.kor+x.eng,x.math))
+
+for value in grade:
+    print(value.name, value.kor, value.eng, value.math)
+
+# Please write your code here.
