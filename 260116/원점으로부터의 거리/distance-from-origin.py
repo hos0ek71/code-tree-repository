094312700e789c13e59e4484ac022f426c31fec3
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-16 22:28:00 +0900

    Title: 원점으로부터의 거리, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260116/원점으로부터의 거리/distance-from-origin.py
@@ -0,0 +1,16 @@
+n = int(input())
+points = [tuple(map(int, input().split())) + (i + 1,) for i in range(n)]
+
+# 함수가 튜플 하나(p)를 받도록 수정
+def 멘하턴(p):
+    x, y = p[0], p[1] # 튜플에서 x, y를 꺼냄
+    
+    return abs(x+y)
+   
+
+# 정렬 실행
+points.sort(key=멘하턴)
+
+# 결과 출력
+for i in range(n):
+    print(points[i][2])
