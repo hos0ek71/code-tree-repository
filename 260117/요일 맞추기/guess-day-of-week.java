0083ac78e077b1f8d9c84ede1731fda947e551c0
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-17 21:06:05 +0900

    Title: 요일 맞추기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260117/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] weekDay = new String[] {"0","Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+        String[] minusWeekDay = new String[] {"0","Sun","Sat","Fri","Thu","Wed","Tue","Mon"};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+        int dis= Math.abs(d2) - Math.abs(d1);
+
+        if (dis > 0)
+            System.out.println(weekDay[dis]);
+        else 
+            System.out.println(minusWeekDay[Math.abs(dis)]);
+
+    }
+}
