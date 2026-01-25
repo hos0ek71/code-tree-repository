42b8b0eb451cc31688a180cda7974faba07a34d3
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-25 15:45:31 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 128ms, Memory: 48MB, Status: Passed - Codetree

--- 260125/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 260125/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -7,28 +7,34 @@ OFFSET = 1000
 arr = [[0] * 2001 for _ in range(2001)]
 
 
-for i in range(1,3):
+for i in range(2):
     ax1 = x1[i]
     ax2 = x2[i]
     ay1 = y1[i]
     ay2 = y2[i]
 
-    for j in range(ax1, ax2+1):
-        for k in range(ay1, ay2+1):
-            arr[j+OFFSET][k+OFFSET] == i
+    for j in range(ax1, ax2):
+        for k in range(ay1, ay2):
+            arr[j+OFFSET][k+OFFSET] = i + 1
 
-maxNum = -1000
+maxX = -2001
+minX = 2001
+maxY = -2001
+minY = 2001
 
+
+found = False
 for i in range(2001):
-    if arr[i][j] == 1 and i > maxNum:
-        maxNum = i
     for j in range(2001):
-        if arr[i][j] == 1 and j > maxNum:
-            maxNum = j
+        if arr[i][j] == 1:
+            found = True
+            if maxX < i: maxX = i
+            if minX > i: minX = i
+            if maxY < j: maxY = j
+            if minY > j: minY = j
+                
+if not found:
+    print(0)
+else:
+    print( (maxX - minX+1)  * (maxY - minY+1))
 
-for i in range(2001):
-    if arr[i][j] == 1 and i > maxNum:
-        maxNum = i
-    for j in range(2001):
-        if arr[i][j] == 1 and j > maxNum:
-            maxNum = j
