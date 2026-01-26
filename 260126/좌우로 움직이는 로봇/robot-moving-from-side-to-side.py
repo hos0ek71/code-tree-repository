1208fd7ad672981cb2110719e95f28717be47b01
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-26 22:51:55 +0900

    Title: 좌우로 움직이는 로봇, Time: 142ms, Memory: 47MB, Status: Passed - Codetree

--- 260126/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 260126/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,28 +1,41 @@
-n, m = map(int, input().split())
+import sys
+
+# 입력 속도를 위해 sys.stdin.readline 사용 권장
+input = sys.stdin.read().split()
+ptr = 0
+
+n, m = int(input[ptr]), int(input[ptr+1])
+ptr += 2
 
 # Process robot A's movements
 t1 = []
-d1= []
+d1 = []
 for _ in range(n):
-    time, direction = input().split()
-    t1.append(int(time))
+    time_val = int(input[ptr])
+    direction = input[ptr+1]
+    t1.append(time_val)
     d1.append(direction)
+    ptr += 2
 
 # Process robot B's movements
-t2= []
+t2 = []
 d2 = []
 for _ in range(m):
-    time, direction = input().split()
-    t2.append(int(time))
+    time_val = int(input[ptr])
+    direction = input[ptr+1]
+    t2.append(time_val)
     d2.append(direction)
+    ptr += 2
 
-# Please write your code here.
+# --- 시뮬레이션 시작 ---
+# 인덱스 에러 방지를 위해 1,000,001로 설정
+arrA = [0] * 1000001
+arrB = [0] * 1000001
+
+# 로봇 A 기록
 timeA = 0
-OFFSET = 50001
-arrA = [0] * 1000000
 cur = 0  
 for i in range(n):
-
     dis = t1[i]
     if d1[i] == 'R':
         for j in range(dis):
@@ -35,11 +48,10 @@ for i in range(n):
             cur -= 1
             arrA[timeA] = cur
 
-arrB = [0] * 1000000
+# 로봇 B 기록
 timeB = 0
 cur = 0
 for i in range(m):
-
     dis = t2[i]
     if d2[i] == 'R':
         for j in range(dis):
@@ -52,36 +64,22 @@ for i in range(m):
             cur -= 1
             arrB[timeB] = cur
 
-
-# 실제로 둘 중 더 오래 움직인 시간까지만 비교하면 됩니다.
+# 전체 시뮬레이션 시간 계산
 max_time = max(timeA, timeB)
 
-# [Padding] A가 먼저 멈췄다면, A의 마지막 위치(arrA[time_a])로 max_time까지 채워줌
+# [Padding] A가 먼저 멈췄다면, A의 마지막 위치로 max_time까지 채워줌
 for i in range(timeA + 1, max_time + 1):
     arrA[i] = arrA[timeA]
 
-# [Padding] B가 먼저 멈췄다면, B의 마지막 위치(arrB[time_b])로 max_time까지 채워줌
-for i in range(timeB + 1, timeB + 1):
+# [Padding] B가 먼저 멈췄다면, B의 마지막 위치로 max_time까지 채워줌 (범위 수정됨)
+for i in range(timeB + 1, max_time + 1):
     arrB[i] = arrB[timeB]
 
-# [비교] 1초부터 max_time까지만 돌리면 됩니다. (100만까지 돌릴 필요 X)
+# [비교] 1초부터 max_time까지만 판정
 cnt = 0
 for i in range(1, max_time + 1):
+    # 조건: 현재는 위치가 같고(==), 바로 직전(i-1)에는 위치가 달랐어야 함
     if arrA[i] == arrB[i] and arrA[i-1] != arrB[i-1]:
         cnt += 1
 
 print(cnt)
-
-
-
-
-    
-
-# 초에 맞게 배열에 저장하고
-# 그 초에 배열에 똑같은 방향이 왔다하면
-# 만약에 직전에 같은 위치에 않았다면 arrA[i-1] !=  arrB[i-1]
-# 0초에는 증가하지않고 그이후 시간에 만남카운트증가
-# 끝까지 다하고 난후에는 만남카운트 증가하면안됨 if i == max continue;
-
-
-
