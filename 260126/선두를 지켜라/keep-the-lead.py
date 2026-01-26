1e75a43d221b3608a1d8e3019bb803ae579006b4
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-26 21:14:15 +0900

    Title: 선두를 지켜라, Time: 104ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260126/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,69 @@
+n, m = map(int, input().split())
+
+# Process A's movements
+v = []
+t = []
+for _ in range(n):
+    vi, ti = map(int, input().split())
+    v.append(vi)
+    t.append(ti)
+
+# Process B's movements
+v2 = []
+t2 = []
+for _ in range(m):
+    vi, ti = map(int, input().split())
+    v2.append(vi)
+    t2.append(ti)
+
+# Please write your code here.
+arrA = [0] * 2001 
+arrB = [0] * 2001
+
+timeA = 0
+spdSum = 0 
+for i in range(n):
+    disA = t[i]
+    spd = v[i]
+    for j in range(disA):
+        timeA += 1
+        spdSum += spd 
+        arrA[timeA] = spdSum
+
+timeB = 0
+spdSum = 0         
+for i in range(m):
+    disB = t2[i]
+    spd = v2[i]
+    for j in range(disB):
+        timeB += 1
+        spdSum += spd 
+        arrB[timeB] = spdSum
+
+maxTime = max(timeA, timeB)
+sundu = 0
+cnt = 0
+
+for i in range(1, maxTime):
+    if arrA[i] > arrB[i]:
+        if sundu == 2:
+            cnt += 1
+        sundu = 1
+        
+    elif arrA[i] < arrB[i]:
+        if sundu == 1:
+            cnt += 1
+        sundu = 2
+
+print(cnt)
+
+
+    # print(f"A : {arrA[i]} ,B : {arrB[i]}" )
+    
+
+    
+
+    
+    
+
+
