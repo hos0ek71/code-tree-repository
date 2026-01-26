a569963bcead4275801dc1b7b8b53df12ea1ee34
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-26 17:19:58 +0900

    Title: 벌금은 누구에게, Time: 181ms, Memory: 12MB, Status: Passed - Codetree

--- 260126/벌금은 누구에게/who-will-pay.java
+++ 260126/벌금은 누구에게/who-will-pay.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 1. 입력 받기 (수정 없음)
         int n = sc.nextInt();
         int m = sc.nextInt();
         int k = sc.nextInt();
@@ -11,22 +12,19 @@ public class Main {
             penalizedPerson[i] = sc.nextInt();
         }
 
-        int[] penaltyCount = new int[n+1];
-        for (int i = 0; i < m; i++){
-            penaltyCount[penalizedPerson[i]]++;
-            
-        }
+        // 2. 로직 수정 (입력받은 배열을 시간순으로 검사)
+        int[] penaltyCount = new int[n + 1];
         int ans = -1;
-        for (int i = 0; i <= n; i++){
-            if(penaltyCount[i] == k ){
-                ans = i;
+        for (int i = 0; i < m; i++) {
+            int person = penalizedPerson[i];
+            penaltyCount[person]++;
+            if (penaltyCount[person] == k) {
+                ans = person;
                 break;
-                
             }
-            
         }
-        System.out.println(ans);
 
-        // Please write your code here.
+        // 3. 결과 출력 (필수!)
+        System.out.println(ans);
     }
 }
