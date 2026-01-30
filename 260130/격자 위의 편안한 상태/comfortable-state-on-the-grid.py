5f7dac0e6b49730f1e3be40187098868a24d8b1f
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-30 17:19:47 +0900

    Title: 격자 위의 편안한 상태, Time: 105ms, Memory: 19MB, Status: Passed - Codetree

--- 260130/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 260130/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,31 +1,31 @@
 n, m = map(int, input().split())
 points = [tuple(map(int, input().split())) for _ in range(m)]
 
-# Please write your code here.
+# 1~n 좌표를 쓰기 위해 n+1 크기
+arr = [[0] * (n+1) for _ in range(n+1)]
 
-arr = [[0] * (m+1) for _ in range(n+1)]
-dx = [0,1,0,-1]
-dy = [1,0,-1,0]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-def isRange(nx, ny, n, m):
-    if 1 <= nx  and nx <= n and 1 <= ny and ny <= m:
-        return True
-    else:
-        return False
+def isRange(x, y):
+    return 1 <= x <= n and 1 <= y <= n
 
-for row, column in points:
-    arr[row][column] = 1
+for row, col in points:
+    # 색칠
+    arr[row][col] = 1
+    
     cnt = 0
+    
+    # 상하좌우 4방향 검사
     for i in range(4):
         nx = row + dx[i]
-        ny = column + dy[i]
-
-        if isRange(nx,ny,n,m) and arr[nx][ny] == 1:
+        ny = col + dy[i]
+        
+        if isRange(nx, ny) and arr[nx][ny] == 1:
             cnt += 1
-
+    
+    # 정확히 3개면 편안한 상태
     if cnt == 3:
         print(1)
     else:
         print(0)
-
-    
