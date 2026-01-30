cc59f848510dd60f67ebab99c95992c4f8a46ca4
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-30 17:14:32 +0900

    Title: 격자 위의 편안한 상태, Time: 112ms, Memory: 20MB, Status: Runtime Error - Codetree

--- 260130/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 260130/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -3,8 +3,7 @@ points = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
 
-arr = [[0] * (m+1) for _ in range(n+1)]
-
+arr = [[0] * (n+1) for _ in range(n+1)]
 
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
