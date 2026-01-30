0779366c6958640cf0ca0990c16d714f13375a2e
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-01-30 15:41:09 +0900

    Title: 되돌아오기 2, Time: 62ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260130/되돌아오기 2/come-back-2.py
@@ -0,0 +1,44 @@
+commands = input()
+
+# Please write your code here.
+
+x = 0 
+y = 0
+
+dx = [-1, 0,1,0]
+dy = [0,1,0,-1]
+
+
+def getDir(direction, dir ):
+    
+    if direction == 'R':
+        dir = (dir + 1) % 4
+    else:
+        dir = ((dir +1) + 2 ) % 4 
+
+    return dir    
+
+prevdir = 0
+cnt = 0
+result = 0
+for i in range(len(commands)):
+    dir = commands[i]
+    
+    
+    if dir == 'F':
+        x = x + dx[prevdir]
+        y = y + dy[prevdir]
+        cnt += 1
+    else:
+        curdir = getDir(dir, prevdir)
+        prevdir = curdir
+        cnt += 1
+
+    if x == 0 and y == 0:
+        result = cnt
+    
+    if x != 0 and y != 0:
+        result = -1
+
+print(result)
+
