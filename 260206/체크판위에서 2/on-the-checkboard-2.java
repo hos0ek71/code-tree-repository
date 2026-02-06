780629e4779aed4578114e072ede1ed6aaa87030
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-02-06 23:03:05 +0900

    Title: 체크판위에서 2, Time: 127ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260206/체크판위에서 2/on-the-checkboard-2.java
+++ 260206/체크판위에서 2/on-the-checkboard-2.java
@@ -15,35 +15,28 @@ public class Main {
             }
         }
 
-        char startColor = grid[0][0];
-        char endColor = grid[R - 1][C - 1];
-
-        int count = 0;
+        int cnt = 0;
 
         // A = (i, j)
         for (int i = 1; i < R - 1; i++) {
             for (int j = 1; j < C - 1; j++) {
 
-                // 시작 -> A 색 조건
-                if (grid[i][j] == startColor) continue;
-
                 // B = (k, l)
-                for (int k = i + 1; k < R - 1; k++) {
-                    for (int l = j + 1; l < C - 1; l++) {
+                for (int k = i + 1; k < R; k++) {
+                    for (int l = j + 1; l < C; l++) {
 
-                        // A -> B 색 조건
-                        if (grid[k][l] == grid[i][j]) continue;
+                        // ❌ B가 도착점이면 안됨
+                        if (k == R - 1 && l == C - 1) continue;
 
-                        // B -> 도착 색 조건
-                        if (grid[k][l] == endColor) continue;
-
-                        // 조건 다 만족하면 경우의 수 1 증가
-                        count++;
+                        // ✅ 색 조건: A와 B 색이 다르고, B와 도착점 색도 달라야 함
+                        if (grid[i][j] != grid[k][l] && grid[k][l] != grid[R - 1][C - 1]) {
+                            cnt++;
+                        }
                     }
                 }
             }
         }
 
-        System.out.println(count);
+        System.out.println(cnt);
     }
 }
