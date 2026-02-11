892cd46b91fc1bab5ec635bf75102b0761a3559c
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-02-11 22:22:38 +0900

    Title: 원 모양으로 되어있는 방, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260211/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,34 @@
+n = int(input())
+a = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+minSum = 10000
+
+for i in range(n):
+    sum = 0
+    idx = 0 
+    for j in range(n):
+        
+        if i + j > n-1:
+            idx = (i + j) - n  
+        else:
+            idx = i + j 
+        
+        # print(i,j, end=",")
+        # print(idx)
+        
+        sum += a[idx] * j 
+
+    if sum < minSum:
+        minSum = sum
+
+print(minSum)
+
+# i = 1
+# (7 * 0) + (8 * 1) + (6 * 2) + (4 * 3) + (4 * 4) = 0 8 12 12 16 
+# [i + j] 0 + [i+j] 1 ... i+j  4
+# (-1 * 4) + (0 * 7) + (1 * 8) + 2 * 6 + 3 * 4
+# i = 2
+# (8 * 0) (6 * 1) (4 * 2) (4 * 3) (7 * 4)
+# [2 + j] * 0 2 + j      
