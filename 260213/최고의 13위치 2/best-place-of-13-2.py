53fd112bb7481d7b3a4d0e31625da4d69cbeff16
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-02-13 21:11:49 +0900

    Title: 최고의 13위치 2, Time: 103ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 260213/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+maxNum = 0
+for i in range(n):
+    for j in range(n-2):
+        if maxNum < (arr[i][j] + arr[i][j+1] + arr[i][j+2]):
+            maxNum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+            secMaxNum = 0 
+            for k in range(i+1, n):
+                for l in range(n-2):
+                    secMaxNum = max(arr[k][l]+arr[k][l+1]+arr[k][l+2], secMaxNum)
+                    
+print(maxNum + secMaxNum)
+        
+    
+
+            
