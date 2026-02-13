255b50556787973b357a4a4f98a4d0b9494d7ed5
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-02-13 16:36:10 +0900

    Title: 숨은 단어 찾기 2, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 260213/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -0,0 +1,25 @@
+N, M = map(int, input().split())
+arr = [input() for _ in range(N)]
+
+# Please write your code here.
+
+directions = [(0,1),(1,0),(-1,1),(1,1)]
+
+def isRange(x, y):
+    return 0 <= x and x < N and 0 <= y and y < M
+
+for i in range(N-2):
+    for j in range(M-2):
+        
+        
+        for k in range(4):
+            for step in range(3):
+
+            nx = j + (dx * step)
+            ny = j + (dy * step)
+
+            if not isRange(nx, ny):
+                break;
+            
+            
+            
