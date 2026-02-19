a5d781c4d347978661a50d4f8d8bc878c4023581
JuHoSoek <86303497+hos0ek71@users.noreply.github.com>
2026-02-19 17:30:40 +0900

    Title: 개발자의 능력 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260219/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,35 @@
+ability = list(map(int, input().split()))
+
+# Please write your code here.
+sum = 0 
+for value in ability:
+    sum += value
+diffteam = 1000000
+
+for i in range(6):
+    for j in range(i+1, 6):
+        
+        for k in range(6):
+            for l in range(k+1, 6):
+
+                if k == i or k == j or l == i or l == j:
+                    continue
+                minteam = 0 
+                maxteam = 0
+
+                team1 = ability[i] + ability[j]
+                team2 = ability[k] + ability[l]
+                team3 = sum - (team1+team2)
+                
+                minteam = min(team1,team2,team3)
+                maxteam = max(team1,team2,team3)
+
+                diffteam = min(diffteam, maxteam - minteam)
+
+print(diffteam)
+
+
+
+
+        
+    
